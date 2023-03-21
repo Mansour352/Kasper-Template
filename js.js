ae739576c442ed393d4297f3e47dc07faa38c562
MansTKD <mansour.hamidi@yahoo.com>
2023-03-22 02:32:00 +0330

    edited some error and add scroll page

---
+++ js.js
@@ -0,0 +1,9 @@
+// Page Scroll
+
+let el = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    el.style.width = `${(scrollTop / height) * 100}%` ;
+});
